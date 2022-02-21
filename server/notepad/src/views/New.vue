2571c992d9588b9a1397e35972daa8ea56920aa3
gyokuro06 <gyokuro06@gmail.com>
2022-02-21 20:24:08 +0900

    Add create new file component (i dont remenmber anything about this)

---
+++ server/notepad/src/views/New.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="new">
+    <h1 class="title">New memo</h1>
+    <DownloadBtn></DownloadBtn>
+    <MdEditor></MdEditor>
+  </div>
+</template>
+
+<script>
+import DownloadBtn from '../components/DownloadButton.vue'
+import MdEditor from '../components/MarkdownEditor.vue'
+
+export default {
+  name: 'new',
+  // data() {
+  //   return {
+  //     memo: { id: Number, body: String }
+  //   }
+  // },
+  components: {
+    DownloadBtn,
+    MdEditor
+  },
+  mounted() {
+    const id = this.$store.getters.maxId + 1;
+    this.$store.commit("lockId", id);
+    console.log("New mounted.(%d)", id);
+  }
+}
+</script>
+
+<style scoped></style>
